b58d6150209cc98d803044ba7c7be6a94a42684f
Denilson Telaroli <ddts80@gmail.com>
2015-11-11 22:19:01 -0200

    Inicial form

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,20 +8,38 @@ angular.module('starter.controllers', ['ng-token-auth'])
     });
 })
 
-.controller('LoginCtrl', function($scope, $auth, $state) {
-  $auth.validateUser().then(function() {
+.controller('LoginCtrl', function($scope, $auth, $state, CepService) {
+
+  $scope.user = {$logged: false};
+  $scope.cep = {value: '', $present: false};
+
+  $auth.validateUser().then(function(user) {
+    $scope.user = user;
+    $scope.user.$logged = true;
     // $state.go('admin.dash');
   });
 
   $scope.login = function(provider) {
     $auth.authenticate(provider)
       .then(function(resp) {
-        $state.go('admin.dash');
+        $scope.user = resp;
+        $scope.user.$logged = true;
+        // $state.go('admin.dash');
       })
       .catch(function(resp) {
-        alert('Erro')
+        alert('Erro');
       });
   };
+
+  $scope.$watch('cep.value', function(cep) {
+    if($scope.cep.value.length == 8) {
+      CepService.get({cep: $scope.cep.value}, function(address) {
+        $scope.cep.$present = true;
+        $scope.user.address = address.logradouro + ', ' + address.bairro + ', ' + address.localidade + ' - ' + address.uf;
+        $scope.user = angular.extend($scope.user, address);
+      });
+    }
+  });
 })
 
 .controller('DashCtrl', function($scope, $auth) {
