a051d1415dc7da6fa1869ec4a1c9035120383346
Denilson Telaroli <ddts80@gmail.com>
2015-11-16 22:26:28 -0200

    API url

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,11 @@
 angular.module('starter.services', ['ngResource'])
 
+.factory('API', function() {
+  return {
+    host: 'https://warm-dusk-4656.herokuapp.com'
+  };
+})
+
 .factory('CepService', function($resource) {
   return $resource('https://viacep.com.br/ws/:cep/json/unicode', {}, {
     get: {
@@ -8,16 +14,16 @@ angular.module('starter.services', ['ngResource'])
   });
 })
 
-.factory('ProfileService', function($resource) {
-  return $resource('http://localhost:3000/profiles/:id.:format', {format: 'json'}, {
+.factory('ProfileService', function($resource, API) {
+  return $resource(API.host + '/profiles/:id.:format', {format: 'json'}, {
     update: {
       method: 'PUT'
     }
   });
 })
 
-.factory('ScheduleService', function($resource) {
-  return $resource('http://localhost:3000/schedules/:id.:format', {format: 'json'}, {
+.factory('ScheduleService', function($resource, API) {
+  return $resource(API.host + '/schedules/:id.:format', {format: 'json'}, {
     update: {
       method: 'PUT'
     }
